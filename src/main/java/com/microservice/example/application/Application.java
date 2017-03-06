951d539617ef6483fe161f3e14456becc739a33b
simo <clouds.simo@gmail.com>
2017-03-06 11:42:01 +0000

    Added instructions

--- src/main/java/com/microservice/example/application/Application.java
+++ src/main/java/com/microservice/example/application/Application.java
@@ -1,22 +1,32 @@
 package com.microservice.example.application;
 
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ApplicationContext;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.util.Arrays;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication(scanBasePackages = "com.microservice.example")
 @EntityScan("com.microservice.example.model")
 @EnableJpaRepositories("com.microservice.example.repository")
+//@EnableCaching
 public class Application {
 
 
     public static void main(String[] args) throws Exception {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
     }
+
+
+//    @Bean
+//    public Config config() {
+//        return new Config().addMapConfig(
+//                new MapConfig()
+//                        .setName("accepted-messages")
+//                        .setEvictionPolicy(EvictionPolicy.LRU)
+//                        .setTimeToLiveSeconds(2400));
+//    }
 }
